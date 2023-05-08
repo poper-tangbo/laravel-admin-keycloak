ca61ebc3acc899247f0c61e32faaddb666c7899f
唐波 <botang@poper.co>
2023-05-08 15:01:09 +0800

    feat:web注册

--- app/Extensions/WebUserProvider.php
+++ app/Extensions/WebUserProvider.php
@@ -2,17 +2,27 @@
 
 namespace App\Extensions;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Log;
 use Vizir\KeycloakWebGuard\Auth\KeycloakWebUserProvider;
 
 class WebUserProvider extends KeycloakWebUserProvider
 {
-    public function retrieveByCredentials(array $credentials)
+    /**
+     * @param array $credentials
+     * @return ?Authenticatable
+     */
+    public function retrieveByCredentials(array $credentials): ?Authenticatable
     {
         $user = $this->newModelQuery()
             ->where('email', $credentials['email'])
             ->first();
         if (empty($user)) {
-            throw new \Exception('User not found');
+            $user = $this->createModel();
+            $user->email = $credentials['email'];
+            $user->name = $credentials['family_name'] . $credentials['given_name'];
+            $user->password = 'no-password';
+            $user->save();
         }
         return $user;
     }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Vizir\KeycloakWebGuard\Facades\KeycloakWeb;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', function () {
+    $url = KeycloakWeb::getRegisterUrl();
+    KeycloakWeb::saveState();
+    return redirect($url);
+})->name('keycloak.register');
 
 Route::middleware('auth')->get('home', function () {
     /** @var User $user */
