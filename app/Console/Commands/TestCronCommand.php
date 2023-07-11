3f2dc4c7796c8e7dd3d3cff17319385348b3b0ca
唐波 <botang@poper.co>
2023-07-11 09:56:06 +0800

    debug:test cron

---
+++ app/Console/Commands/TestCronCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCronCommand extends Command
+{
+    protected $signature = 'test:cron';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $this->line('test cron job, now at ' . now()->toDateTimeString());
+    }
+}
